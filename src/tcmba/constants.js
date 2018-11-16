024d2420c8587969dfbab768ad11de77793c7bf5
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-11-15 23:23:19 -0300

    create file with constants of tcmba

---
+++ src/tcmba/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  URLS: {
+    SALARIES: 'http://www.tcm.ba.gov.br/portal-da-cidadania/pessoal/'
+  },
+  SELECTORS: {
+    CITIES: '#formConsulta #municipios',
+    MONTHS: '#formConsulta #mes',
+    YEARS: '#formConsulta #ano'
+  }
+}
