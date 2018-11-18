53060dd3814ccae577b7e5bb6e81736c8c749d67
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-11-18 10:24:16 -0300

    create dedicated file for mock values for tests

---
+++ test/mock-options.js
@@ -0,0 +1,23 @@
+module.exports = {
+  city: {
+    name: 'municipios',
+    value: '2910800   ',
+    text: 'Feira de Santana'
+  },
+  entity: {
+    name: 'entidades',
+    text: 'Camara Municipal de FEIRA DE SANTANA',
+    value: '544'
+  },
+  year: {
+    name: 'ano',
+    text: '2018',
+    value: '2018'
+  },
+  month: {
+    name: 'mes',
+    text: 'JANEIRO',
+    value: '1'
+  },
+  paymentKey: '2910800_544_2018_1'
+}
--- test/tcmba.test.js
+++ test/tcmba.test.js
@@ -2,27 +2,7 @@ var assert = require('assert')
 const FormOptions = require('../src/tcmba/form-options/api')
 const Entities = require('../src/tcmba/entities/api')
 const Payments = require('../src/tcmba/payments/api')
-
-const city = {
-  name: 'municipios',
-  value: '2910800   ',
-  text: 'Feira de Santana'
-}
-const entity = {
-  name: 'entidades',
-  text: 'Camara Municipal de FEIRA DE SANTANA',
-  value: '544'
-}
-const year = {
-  name: 'ano',
-  text: '2018',
-  value: '2018'
-}
-const month = {
-  name: 'mes',
-  text: 'JANEIRO',
-  value: '1'
-}
+const MockOptions = require('./mock-options')
 
 describe('Form Options', function() {
   describe('#api.get()', function() {
@@ -42,7 +22,7 @@ describe('Entities', function() {
   describe('#api.get()', function() {
     it('Get all entities from a given city', function(done) {
       ;(async () => {
-        let result = await Entities.get({ cityKey: city.value })
+        let result = await Entities.get({ cityKey: MockOptions.city.value })
         assert.equal(result.length, 9)
         done()
       })().catch(error => done(error))
@@ -54,7 +34,12 @@ describe('Payments', function() {
   describe('#api.getRaw()', function() {
     it('Get content of payments page for a combination of city, entity, year and month', function(done) {
       ;(async () => {
-        let result = await Payments.getRaw({ city, entity, year, month })
+        let result = await Payments.getRaw({
+          city: MockOptions.city,
+          entity: MockOptions.entity,
+          year: MockOptions.year,
+          month: MockOptions.month
+        })
         assert.ok(result.length > 0)
         done()
       })().catch(error => done(error))
